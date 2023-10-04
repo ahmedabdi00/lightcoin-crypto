885c622904e2173bade65eb61c80634a634461c3
Ahmed Abdi <a.abdi11965@gmail.com>
2023-10-04 11:58:23 -0400

    Dry

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = []; // Initialize an empty array to store transactions
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+
+  get value() {
+    return this.amount;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    if (this.isAllowed()) {
+      this.account.addTransaction(this);
+    } else {
+      console.log("Transaction denied: Insufficient balance.");
+    }
   }
 
+  isAllowed() {
+    return true; // This is a placeholder; you can implement validation here.
+  }
+}
+
+class Deposit extends Transaction {
+
+  isAllowed() {
+    return true; // Deposits are always allowed
+  }
+
+  commit() {
+    super.commit(); // Call the commit method of the parent class to add the transaction
+    this.account.addTransaction(this); // Add the transaction to the account
+  }
 }
 
 
+class Withdrawal extends Transaction {
 
+  isAllowed() {
+    return this.account.balance >= this.amount;
+  }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+  commit() {
+    if (this.isAllowed()) {
+      this.account.balance -= this.amount;
+      super.commit();
+    } else {
+      console.log("Transaction denied: Insufficient balance.");
+    }
+  }
+}
 
-t1 = new Withdrawal(50.25);
+// Create an account
+const myAccount = new Account("snow-patrol");
+
+// Create and commit transactions
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
